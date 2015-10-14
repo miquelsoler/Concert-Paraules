5699ff51b7cbf1b49fdb6230bf84a9a2f696854c
Miquel Soler <miquelangelsoler@gmail.com>
2015-10-14 17:10:44 +0200

    Squashed 'src/External/PMAudioAnalysis/' changes from e52d180..f35e3a0
    f35e3a0 prova amb subtress
    e152736 Working on listener for audio analysis, pt 2.
    678da7b Working on listener for audio changes.
    
    git-subtree-dir: src/External/PMAudioAnalysis
    git-subtree-split: f35e3a037303cd071d1b96846509a8e812b316b4

---
+++ PMAudioOutParams.h
@@ -0,0 +1,37 @@
+//
+//  PMAudioOutParams.h
+//  PMConcertParaules
+//
+//  Created by Miquel Àngel Soler on 13/10/15.
+//  hola
+//
+
+#ifndef PMAudioOutParams_h
+#define PMAudioOutParams_h
+
+struct baseAudioOutParams
+{
+    int deviceID;
+    int channel;
+};
+
+struct pitchParams : public baseAudioOutParams
+{
+    float freq;
+    float midiNote;
+    float midiNoteNoOctave;
+};
+
+struct onsetParams : public baseAudioOutParams
+{
+};
+
+struct fftBandsParams : public baseAudioOutParams
+{
+    float low;
+    float mid;
+    float high;
+};
+
+
+#endif /* PMAudioOutParams_h */
