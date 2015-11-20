b4d366d9f7f7bbe469fc655f6eb7b7fe21d235fd
Miquel Soler <miquelangelsoler@gmail.com>
2015-11-20 15:03:32 +0100

    PMUICanvasAudioSettings work in progress.

--- src/UICanvas/PMUICanvasAudioSettings.cpp
+++ src/UICanvas/PMUICanvasAudioSettings.cpp
@@ -4,6 +4,34 @@
 
 #include "PMUICanvasAudioSettings.h"
 
+static const unsigned int PRESETSMATRIX_NUMROWS = 2;
+static const unsigned int PRESETSMATRIX_NUMCOLS = 5;
+
+static const string STR_PRESETS = "PRESETS";
+static const string STR_PRESETS_INFO = "Click to LOAD | Shift+Click to SAVE";
+
+static const string STR_PITCH = "PITCH";
+static const string STR_PITCH_MIN_MIDI_NOTE = "Min MIDI Note";
+static const string STR_PITCH_MAX_MIDI_NOTE = "Max MIDI Note";
+
+static const string STR_ENERGY = "ENERGY";
+static const string STR_ENERGY_MIN = "Min";
+static const string STR_ENERGY_MAX = "Max";
+
+static const string STR_SILENCE = "SILENCE";
+static const string STR_SILENCE_THRESHOLD = "Threshold (dB)";
+static const string STR_SILENCE_QUEUE_LENGTH = "Queue Length";
+
+static const string STR_ONSETS = "ONSETS";
+static const string STR_ONSETS_THRESHOLD = "Silence Threshold";
+static const string STR_ONSETS_ALPHA = "Alpha";
+
+static const string STR_SMOOTHING = "SMOOTHING";
+static const string STR_SMOOTHING_DELTA = "Delta";
+
+static const string STR_MELBANDS = "MEL BANDS";
+static const string STR_MELBANDS_NUMBANDS = "Num Bands";
+
 PMUICanvasAudioSettings::PMUICanvasAudioSettings(string title, int headerFontSize) : PMBaseUICanvas(title, headerFontSize)
 {
 }
@@ -14,44 +42,52 @@ void PMUICanvasAudioSettings::init(int posX, int posY, bool autosize, int width,
 
     settings = &PMSettingsManagerAudioAnalyzers::getInstance();
 
-    addLabel("PITCH");
+    addLabel(STR_PRESETS);
+    addLabel(STR_PRESETS_INFO, OFX_UI_FONT_SMALL);
+    setGlobalButtonDimension(32);
+    presetsMatrix = addToggleMatrix(STR_PRESETS, PRESETSMATRIX_NUMROWS, PRESETSMATRIX_NUMCOLS);
+    presetsMatrix->setAllowMultiple(false);
+    addSpacer();
+
+    addLabel(STR_PITCH);
     minMidiValue = settings->getMinPitchMidiNote();
     maxMidiValue = settings->getMaxPitchMidiNote();
-    minMidiSlider = addSlider("Min MIDI Note", 0, 100, &minMidiValue);
-    maxMidiSlider = addSlider("Max MIDI Note", 0, 100, &maxMidiValue);
+    minMidiSlider = addSlider(STR_PITCH_MIN_MIDI_NOTE, 0, 100, &minMidiValue);
+    maxMidiSlider = addSlider(STR_PITCH_MAX_MIDI_NOTE, 0, 100, &maxMidiValue);
     addSpacer();
 
-    addLabel("ENERGY");
+    addLabel(STR_ENERGY);
     minEnergyValue = settings->getMinEnergy();
     maxEnergyValue = settings->getMaxEnergy();
-    minEnergySlider = addSlider("Min", 0, 10, &minEnergyValue);
-    maxEnergySlider = addSlider("Max", 0, 10, &maxEnergyValue);
+    minEnergySlider = addSlider(STR_ENERGY_MIN, 0, 10, &minEnergyValue);
+    maxEnergySlider = addSlider(STR_ENERGY_MAX, 0, 10, &maxEnergyValue);
     addSpacer();
 
-    addLabel("SILENCE");
+    addLabel(STR_SILENCE);
     silenceThresholdValue = settings->getSilenceThreshold();
     silenceQueueValue = settings->getSilenceQueueLength();
-    silenceThresholdSlider = addIntSlider("Threshold (dB)", -60, -1, &silenceThresholdValue);
-    silenceQueueSlider = addIntSlider("Queue Length", 0, 200, &silenceQueueValue);
+    silenceThresholdSlider = addIntSlider(STR_SILENCE_THRESHOLD, -60, -1, &silenceThresholdValue);
+    silenceQueueSlider = addIntSlider(STR_SILENCE_QUEUE_LENGTH, 0, 200, &silenceQueueValue);
     addSpacer();
 
-    addLabel("ONSETS");
+    addLabel(STR_ONSETS);
     onsetThresholdValue = settings->getOnsetsThreshold();
     onsetAlphaValue = settings->getOnsetsAlpha();
-    onsetThresholdSlider = addSlider("Silence Threshold", 0, 10, &onsetThresholdValue);
-    onsetAlphaSlider = addSlider("Alpha", 0, 1, &onsetAlphaValue);
+    onsetThresholdSlider = addSlider(STR_ONSETS_THRESHOLD, 0, 10, &onsetThresholdValue);
+    onsetAlphaSlider = addSlider(STR_ONSETS_ALPHA, 0, 1, &onsetAlphaValue);
     addSpacer();
 
-    addLabel("SMOOTHING");
+    addLabel(STR_SMOOTHING);
     smoothingDeltaValue = settings->getSmoothingDelta();
-    smoothingDeltaSlider = addSlider("Delta", 0, 1, &smoothingDeltaValue);
+    smoothingDeltaSlider = addSlider(STR_SMOOTHING_DELTA, 0, 1, &smoothingDeltaValue);
     smoothingDeltaSlider->setLabelPrecision(10);
     addSpacer();
 
-    addLabel("MEL BANDS");
+    addLabel(STR_MELBANDS);
     numMelBandsValue = settings->getNumMelBands();
-    numMelBandsSlider = addIntSlider("Num Bands", 1, 32, &numMelBandsValue);
+    numMelBandsSlider = addIntSlider(STR_MELBANDS_NUMBANDS, 1, 32, &numMelBandsValue);
 
+    ofAddListener(newGUIEvent, this, &PMUICanvasAudioSettings::handleEvents);
 
     if (autosize) autoSizeToFitWidgets();
 }
@@ -64,4 +100,9 @@ void PMUICanvasAudioSettings::clear()
 
 void PMUICanvasAudioSettings::handleEvents(ofxUIEventArgs &e)
 {
+    string name = e.getName();
+    int kind = e.getKind();
+
+    if (ofIsStringInString(name, STR_PRESETS))
+        cout << "Got event from: " << name << endl;
 }
