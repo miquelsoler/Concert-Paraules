5ac8a834d1cd3a3f01c82c780fa785d4fbaa820f
Miquel Soler <miquelangelsoler@gmail.com>
2015-12-29 19:40:59 +0100

    Changed strings to constants in PMUICanvasRibbonRenderer (while working on radio buttons for color modes).

--- src/UICanvas/Renderers/PMUICanvasRibbonRenderer.cpp
+++ src/UICanvas/Renderers/PMUICanvasRibbonRenderer.cpp
@@ -4,9 +4,28 @@
 
 #include "PMUICanvasRibbonRenderer.h"
 
+static const string STR_BRUSH_BASIC                 = "BRUSH (BASIC)";
+static const string STR_BRUSH_BASIC_NUMPAINTERS     = "Number of Painters";
+static const string STR_BRUSH_BASIC_WIDTH           = "Stroke Width";
+static const string STR_BRUSH_BASIC_SPEED           = "Stroke Speed";
+static const string STR_BRUSH_BASIC_ENABLEBOUNCE    = "Enable Bounce";
+static const string STR_BRUSH_ADVANCED              = "BRUSH (ADVANCED)";
+static const string STR_BRUSH_ADVANCED_DIVISIONS    = "Number of Divisions";
+static const string STR_BRUSH_ADVANCED_EASE         = "Ease";
+static const string STR_BRUSH_ADVANCED_MAXVERT      = "Path Max Vertices";
+static const string STR_BRUSH_COLORMODE             = "BRUSH COLOR MODE";
+static const string STR_BRUSH_COLORMODE_MFIXEDCOLOR = "Fixed Color";
+static const string STR_BRUSH_COLORMODE_MGRSPEED    = "Gradient (with Speed)";
+static const string STR_BRUSH_COLORMODE_MGREDGES    = "Gradient (edges)";
+static const string STR_BRUSH_COLORMODE_GRIMAGE     = "Gradient";
+static const string STR_BRUSH_COLORMODE_GRID        = "Gradient ID";
+static const string STR_BRUSH_COLORMODE_GRSPEED     = "Gradient Speed";
+static const string STR_BRUSH_COLORMODE_R           = "Fixed Red";
+static const string STR_BRUSH_COLORMODE_G           = "Fixed Green";
+static const string STR_BRUSH_COLORMODE_B           = "Fixed Blue";
+
 PMUICanvasRibbonRenderer::PMUICanvasRibbonRenderer(PMUIRendererType type, string title, int headerFontSize) : PMUICanvasBaseRenderer(type, title, headerFontSize)
 {
-
 }
 
 void PMUICanvasRibbonRenderer::init(int posX, int posY, bool autosize, int width, int height)
@@ -19,24 +38,40 @@ void PMUICanvasRibbonRenderer::init(int posX, int posY, bool autosize, int width
     setGlobalSliderHeight(10);
     setGlobalButtonDimension(14);
 
-    addSpacer();
-    addLabel("RIBBON BASIC");
-    addIntSlider("Number of painters", 1, 100, &numPainters);
-    addIntSlider("Stroke width", 1, 10, &strokeWidth);
-    addSlider("Speed", 1.01f, 10, &speed);
-    addToggle("Enable bounce", &enableBounce);
-    addImageSampler("Gradient", &gradientImage);
-    addIntSlider("Gradient Id", 1, 4, &gradientId);
-    addIntSlider("Gradient Speed", 1, 500, &gradientSpeed);
-    addIntSlider("Ribbon R", 0, 255, &colorR);
-    addIntSlider("Ribbon G", 0, 255, &colorG);
-    addIntSlider("Ribbon B", 0, 255, &colorB);
+    { // Brush (basic)
+        addSpacer();
+        addLabel(STR_BRUSH_BASIC);
+        addIntSlider(STR_BRUSH_BASIC_NUMPAINTERS, 1, 100, &numPainters);
+        addIntSlider(STR_BRUSH_BASIC_WIDTH, 1, 10, &strokeWidth);
+        addSlider(STR_BRUSH_BASIC_SPEED, 1.01f, 10, &speed);
+        addToggle(STR_BRUSH_BASIC_ENABLEBOUNCE, &enableBounce);
+    }
 
-    addSpacer();
-    addLabel("RIBBON ADVANCED");
-    addSlider("Divisions", 0.001, 4.5, &divisions);
-    addSlider("Ease", 0.001, 10.0, &ease);
-    addIntSlider("Path Max vertices",3, 4000, &pathMaxVertices);
+    { // Brush (advanced)
+        addSpacer();
+        addLabel(STR_BRUSH_ADVANCED);
+        addSlider(STR_BRUSH_ADVANCED_DIVISIONS, 0.001, 4.5, &divisions);
+        addSlider(STR_BRUSH_ADVANCED_EASE, 0.001, 10.0, &ease);
+        addIntSlider(STR_BRUSH_ADVANCED_MAXVERT,3, 4000, &pathMaxVertices);
+    }
+
+    { // Color mode
+        addSpacer();
+        addLabel(STR_BRUSH_COLORMODE);
+
+//        vector<string> colorModesNames;
+//        colorModesNames.push_back(STR_BRUSH_COLORMODE_MFIXEDCOLOR);
+//        colorModesNames.push_back(STR_BRUSH_COLORMODE_MGRSPEED);
+//        colorModesNames.push_back(STR_BRUSH_COLORMODE_MGREDGES);
+//    addRadio
+
+        addImageSampler(STR_BRUSH_COLORMODE_GRIMAGE, &gradientImage);
+        addIntSlider(STR_BRUSH_COLORMODE_GRID, 1, 4, &gradientId);
+        addIntSlider(STR_BRUSH_COLORMODE_GRSPEED, 1, 500, &gradientSpeed);
+        addIntSlider(STR_BRUSH_COLORMODE_R, 0, 255, &colorR);
+        addIntSlider(STR_BRUSH_COLORMODE_G, 0, 255, &colorG);
+        addIntSlider(STR_BRUSH_COLORMODE_B, 0, 255, &colorB);
+    }
 
     if (autosize) autoSizeToFitWidgets();
 
